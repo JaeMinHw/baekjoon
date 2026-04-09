e6ff69f566a1b1a9bcd16e4cb27a703186f26a17
JaeMinHW <bosik99@naver.com>
2026-04-09 10:03:15 +0900

    0409 - solve 10828

---
+++ 0409/10828.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+
+command_list = ['top', 'size','empty','pop',]
+
+N = int(input())
+arr = []
+for i in range(N):
+
+    command = list(input().split())
+
+    if command[0] in command_list :
+        len_arr = len(arr)
+        if command[0] == 'top' :
+            if len_arr != 0 :
+                print(arr[len_arr - 1])
+            else :
+                print("-1")
+
+        elif command[0] == 'size' :
+            print(len_arr)
+        elif command[0] == 'empty':
+            if len_arr != 0 :
+                print("0")
+            else :
+                print("1")
+        elif command[0] == 'pop':
+            if len_arr != 0 :
+                pop_arr = arr.pop()
+                print(pop_arr)
+            else :
+                print("-1")
+
+
+    else :
+        arr.append(command[1])
