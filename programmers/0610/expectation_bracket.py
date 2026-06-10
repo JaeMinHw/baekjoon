d375084edc5170813ce6cf2728992aeadf1c3249
jaeminHW <bosik99@naver.com>
2026-06-10 09:46:53 +0900

    0610 solve expectation bracket

---
+++ programmers/0610/expectation_bracket.py
@@ -0,0 +1,18 @@
+def solution(n,a,b):
+    answer = 0
+    
+    while a != b:
+        if a %2 != 0:
+            a = (a+1)//2
+        else :
+            a = (a+1) // 2
+        if b %2 != 0:
+            b = (b+1)//2
+        else :
+            b = (b+1) // 2
+        answer += 1
+            
+    return answer
+
+
+print(solution(8, 4, 7))
